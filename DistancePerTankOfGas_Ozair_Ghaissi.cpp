fef00c84311284bf3e86716148f2d462b779e37e
ozair <ozair@OZAIR>
2024-10-03 05:25:23 -0400

    Add project files.

---
+++ DistancePerTankOfGas_Ozair_Ghaissi.cpp
@@ -0,0 +1,29 @@
+/*
+Title Distance Per Tank of Gas
+File name DistancePerTankOfGas_Ozair_Ghaissi.cpp
+Programmer: Ozair Ghaissi
+Date: 10/03/2024
+
+
+
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	const double gallon = 15.0;
+	const double miles = 375.0; 
+	double mpg;		
+
+	mpg = miles / gallon;
+
+	cout << "Miles per gallon" << mpg << endl; 
+
+	return 0;
+
+  
+}
+
